cd2d9c80da8a52d3d73da06332832e0a7c5cdd31
Hawmex <hawmed.a@gmail.com>
2021-08-05 11:29:56 +0430

    added divider widget

---
+++ src/divider/divider.ts
@@ -0,0 +1,27 @@
+import { Nexwidget, css } from 'nexwidget';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'divider-widget': DividerWidget;
+  }
+}
+
+export class DividerWidget extends Nexwidget {
+  static get styles(): CSSStyleSheet[] {
+    return [
+      ...super.styles,
+      css`
+        :host {
+          width: 100%;
+          display: block;
+          width: 100%;
+          height: 1px;
+          background: var(--onSurfaceColor);
+          opacity: 0.08;
+        }
+      `,
+    ];
+  }
+}
+
+DividerWidget.register('divider-widget');
