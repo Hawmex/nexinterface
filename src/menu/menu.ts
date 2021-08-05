4c56cef0b95ff7a789bc7126d65d11cae57eb9d2
Hawmex <hawmed.a@gmail.com>
2021-08-05 13:02:51 +0430

    added menu container widget

---
+++ src/menu/menu.ts
@@ -0,0 +1,32 @@
+import { css, html, Nexwidget, NexwidgetTemplate } from 'nexwidget';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'menu-container-widget': MenuContainerWidget;
+  }
+}
+
+export class MenuContainerWidget extends Nexwidget {
+  static get styles(): CSSStyleSheet[] {
+    return [
+      ...super.styles,
+      css`
+        :host {
+          width: 100%;
+          display: grid;
+          grid-auto-flow: row;
+          padding: 8px;
+          gap: 8px;
+          box-sizing: border-box;
+          background: var(--surfaceColor);
+        }
+      `,
+    ];
+  }
+
+  get template(): NexwidgetTemplate {
+    return html`<slot></slot>`;
+  }
+}
+
+MenuContainerWidget.register('menu-container-widget');
