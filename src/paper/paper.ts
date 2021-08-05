aae7faf055f1c777b49c5aaec7f0a0d49eefaea7
Hawmex <hawmed.a@gmail.com>
2021-08-05 11:30:24 +0430

    added paper widget

---
+++ src/paper/paper.ts
@@ -0,0 +1,40 @@
+import { Nexwidget, css, html, NexwidgetTemplate } from 'nexwidget';
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'paper-widget': PaperWidget;
+  }
+}
+
+export class PaperWidget extends Nexwidget {
+  static get styles(): CSSStyleSheet[] {
+    return [
+      ...super.styles,
+      css`
+        :host {
+          display: flex;
+          flex-direction: column;
+          background: var(--surfaceColor);
+          color: var(--onSurfaceColor);
+          border-radius: 8px;
+          box-sizing: border-box;
+          overflow: hidden;
+          box-shadow: var(--shadowLvl1);
+          width: calc(100% - 32px);
+          margin: 0px auto;
+        }
+
+        :host([full-width]) {
+          border-radius: 0px;
+          width: 100%;
+        }
+      `,
+    ];
+  }
+
+  get template(): NexwidgetTemplate {
+    return html`<slot></slot>`;
+  }
+}
+
+PaperWidget.register('paper-widget');
