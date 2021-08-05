6221213651e6a35afa2219d94505c1a09fe665d2
Hawmex <hawmed.a@gmail.com>
2021-08-05 11:30:39 +0430

    added screen base class

---
+++ src/screen/screen.ts
@@ -0,0 +1,32 @@
+import { Nexwidget, css, NexwidgetAnimation } from 'nexwidget';
+
+export class Screen extends Nexwidget {
+  static get styles(): CSSStyleSheet[] {
+    return [
+      ...super.styles,
+      css`
+        :host {
+          display: grid;
+          grid-auto-flow: row;
+          gap: 16px;
+          padding: 16px 0px;
+          box-sizing: border-box;
+        }
+      `,
+    ];
+  }
+
+  get mountAnimation(): NexwidgetAnimation {
+    return [
+      [
+        { opacity: '0', transform: 'scale(0.9)' },
+        { opacity: '1', transform: 'scale(1)' },
+      ],
+      {
+        duration: Number(this.getCSSProperty('--durationLvl2').replace('ms', '')),
+        easing: this.getCSSProperty('--deceleratedEase'),
+        fill: 'forwards',
+      },
+    ];
+  }
+}
